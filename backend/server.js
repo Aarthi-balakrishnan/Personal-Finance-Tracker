2a80742650ad6ec247b708305934bfc88ad98f78
Aarthi <aarthibalakrishnan06@gmail.com>
2026-09-18 23:00:47 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,107 +3,39 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require("path");
-
-const authRoutes =
-    require("./routes/auth");
-
-const expenseRoutes =
-    require("./routes/expenses");
-
-const budgetRoutes =
-    require("./routes/budget");
-
-const savingsRoutes =
-    require("./routes/savings");
+const authRoutes =require("./routes/auth");
+const expenseRoutes =require("./routes/expenses");
+const budgetRoutes = require("./routes/budget");
+const savingsRoutes =require("./routes/savings");
+const PORT =process.env.PORT||5000;
 
 dotenv.config();
-
 const app = express();
 
-
-// ===============================
-// MIDDLEWARE
-// ===============================
-
 app.use(cors());
-
 app.use(express.json());
-
-
-// ===============================
-// API ROUTES
-// ===============================
-
-app.use(
-    "/api/auth",
-    authRoutes
-);
-
-app.use(
-    "/api/expenses",
-    expenseRoutes
-);
-
-app.use(
-    "/api/budget",
-    budgetRoutes
-);
-
-app.use(
-    "/api/savings",
-    savingsRoutes
-);
-// ===============================
-// FRONTEND
-// ===============================
-
-// Serve frontend folder
-app.use(
-    express.static(
-        path.join(__dirname, "../frontend")
-    )
-);
-
-// ===============================
-// HOME PAGE
-// ===============================
-
+app.use("/api/auth",authRoutes);
+app.use("/api/expenses",expenseRoutes);
+app.use("/api/budget", budgetRoutes);
+app.use("/api/savings",savingsRoutes);
+app.use(express.static(path.join(__dirname, "../frontend")));
 app.get("/", (req, res) => {
-
-    res.sendFile(
+ res.sendFile(
         path.join(
             __dirname,
-            "../frontend/index.html"
-        )
-    );
-
+            "../frontend/index.html"));
 });
-
-
-// ===============================
-// MONGODB
-// ===============================
-
 mongoose
     .connect(process.env.MONGO_URI)
-
     .then(() => {
-
         console.log(
             "MongoDB connected"
         );
-
-        app.listen(
-            process.env.PORT,
-            () => {
-
-                console.log(
-                    `Server running on http://localhost:${process.env.PORT}`
-                );
-
-            }
+    app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});    
+    }
         );
-
     })
 
     .catch((error) => {
